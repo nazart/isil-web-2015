1da3cddc42a14732e79e61f180a2ae14bd14958d
nazart <nazartj@gmail.com>
2015-09-09 14:07:45 -0500

    practica de las clases

---
+++ Clase4/practica/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Primera Practica</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="pregunta1.php">Pregunta 1</a></li>
+            <li><a href="pregunta2.php">Pregunta 2</a></li>
+            <li><a href="pregunta3.php">Pregunta 3</a></li>
+            <li><a href="pregunta4.php">Pregunta 4</a></li>
+            <li><a href="pregunta5.php">Pregunta 5</a></li>
+        </ul>
+    </body>
+</html>
