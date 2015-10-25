b06edddad485dda477c045ea25b205b0ea79570d
nazart <nazartj@gmail.com>
2015-10-25 10:12:48 -0500

    taller 3 entidad curso

---
+++ taller3/app/entity/EntityCurso.php
@@ -0,0 +1,73 @@
+<?php
+require_once realpath(__DIR__ . '/../model/ModelCurso.php');
+
+class EntityCurso {
+
+    protected $_id;
+    protected $_nombre;
+    protected $_credito;
+    protected $_activo;
+
+    function indentify($id) {
+        $modelCurso = new ModelCurso();
+        $data = $modelCurso->getCurso($id);
+        $this->_id = $data['curso_id'];
+        $this->_nombre = $data['curso_nombre'];
+        $this->_credito = $data['curso_credito'];
+        $this->_activo = $data['curso_fecha'];
+        return $data;
+    }
+
+    function getId() {
+        return $this->_id;
+    }
+
+    function setNombre($nombre) {
+        $this->_nombre = $nombre;
+    }
+
+    function getNombre() {
+        return $this->_nombre;
+    }
+
+    function getCredito() {
+        return $this->_credito;
+    }
+
+    function setCredito($credito) {
+        $this->_credito = $credito;
+    }
+
+    function getActivo() {
+        return $this->_activo;
+    }
+
+    function setActivo($activo) {
+        $this->_activo = $activo;
+    }
+
+
+    function save() {
+        $modelCurso = new ModelCurso();
+        $data['curso_nombre'] = $this->_nombre;
+        $data['curso_credito'] = $this->_credito;
+        $data['curso_flag_activo'] = $this->_activo;
+        $data['curso_correo'] = $this->_correo;
+        if ($this->_id != '') {
+            $modelCurso->updateCurso($data, $this->_id);
+        } else {
+            $modelCurso->insertCurso($data);
+        }
+    }
+    
+    function delete() {
+        $modelCurso = new ModelCurso();
+        return $modelCurso->deleteCurso($this->_id);
+    }
+
+    static function getAllCurso() {
+        $modelCurso = new ModelCurso();
+        return $modelCurso->getAllCurso();
+    }
+
+}
