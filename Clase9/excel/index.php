da1aa81f3c083230fc9c63e0d8c31c6687a40de2
nazart <nazartj@gmail.com>
2015-11-02 20:17:29 -0500

    phpexcel

--- Clase9/excel/index.php
+++ Clase9/excel/index.php
@@ -12,19 +12,44 @@ date_default_timezone_set('Europe/London');
 
 <title>PHPExcel Reader Example #01</title>
 
-</head>
+</head> 
 <body>
 
 <h1>PHPExcel Leer Archivo</h1>
 <h2>Usando PHPExcel_IOFactory::load()</h2>
 <?php
 include_once './PHPExcel/PHPExcel.php';
-$inputFileName = 'alumnos.xlsx';
+$inputFileName = realpath((__DIR__).'/alumnos.xlsx');
 echo 'Cargando el archivo ',pathinfo($inputFileName,PATHINFO_BASENAME),' usando IOFactory para identificar el tipo de formato<br />';
 $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
 echo '<hr />';
+$sheet = $objPHPExcel->getSheet(0);
+$highestRow = $sheet->getHighestRow();/*cantidad de filas que se esta barriendo*/
+$highestColumn = 'c';
+$arrayErrores = array();/*almacenar los errores*/
+for ($row = 2; $row <= $highestRow; $row++) {
+    /*me devuelve cada fila*/
+    $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+    $data[] = $rowData[0];/*obtengo la fila sin indices*/
+        foreach ($rowData[0] as $indice => $valorIndice) {/*leer cada valor de la fila actual*/
+        $col = ($indice + 1);
+        if ($col == 3) { /*en la columna 3 ejecuta esta condicion*/
+            if(!filter_var($valorIndice, FILTER_VALIDATE_EMAIL)){
+                $arrayErrores[] = 'El correo es invalido en la columna:'.$col.' de la fila :'.$row.' :'.$valorIndice."\n";
+            }
+        }
+    }
+}
 $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-print_r($sheetData);
+if(!empty($arrayErrores)){
+    foreach ($arrayErrores as $index){
+        echo '<p>'.$index;
+    }
+}else{
+    echo '<p>Archivo correcto';
+    
+}
+//print_r($sheetData);
 ?>
 <body>
 </html>
--- Clase9/excel/index2.php
+++ Clase9/excel/index2.php
@@ -1,16 +1,25 @@
 <?php
-
 include_once './PHPExcel/PHPExcel.php';
 $objPHPExcel = new PHPExcel();
 $objPHPExcel->setActiveSheetIndex(0);
 $sheet = $objPHPExcel->getActiveSheet();
 $row = 1;
-$sheet->SetCellValue('A' . $row, '#');
-$sheet->SetCellValue('B' . $row, 'Columna 1');
-$sheet->SetCellValue('C' . $row, 'Columna 2');
-$sheet->SetCellValue('D' . $row, 'Columna 3');
-$sheet->SetCellValue('E' . $row, 'Columna 4');
-$sheet->SetCellValue('F' . $row, 'Columna 5');
+
+$sheet->SetCellValue('A' . $row, 'numero');
+$sheet->SetCellValue('B' . $row, 'Valor 1');
+$sheet->SetCellValue('C' . $row, 'valor 2');
+$sheet->SetCellValue('D' . $row, 'valor 3');
+$sheet->SetCellValue('E' . $row, 'valor 4');
+$sheet->SetCellValue('F' . $row, 'valor 5');
+
+for ($row = 2; $row <= 10; $row++) {
+    $sheet->SetCellValue('A' . $row, ($row - 1));
+    $sheet->SetCellValue('B' . $row, 'Columna 1' . $row);
+    $sheet->SetCellValue('C' . $row, 'Columna 2' . $row);
+    $sheet->SetCellValue('D' . $row, 'Columna 3' . $row);
+    $sheet->SetCellValue('E' . $row, 'Columna 4' . $row);
+    $sheet->SetCellValue('F' . $row, 'Columna 5' . $row);
+}
 
 $sheet->getColumnDimension('A')->setAutoSize(true);
 $sheet->getColumnDimension('B')->setAutoSize(true);
@@ -18,6 +27,7 @@ $sheet->getColumnDimension('C')->setAutoSize(true);
 $sheet->getColumnDimension('D')->setAutoSize(true);
 $sheet->getColumnDimension('E')->setAutoSize(true);
 $sheet->getColumnDimension('F')->setAutoSize(true);
+
 header("Pragma: public");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
